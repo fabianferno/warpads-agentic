13540ada55a632b66316520d0eb526c5d912c0bb
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2025-02-08 00:51:08 +0530

    fix: url fixes

--- indexer/src/utilities/IncentiveCalculator.ts
+++ indexer/src/utilities/IncentiveCalculator.ts
@@ -50,7 +50,7 @@ export const calculateIncentive = async (id: number) => {
         user_prompt:
           "Just navigate the twitter url and collect the impressions like No of Likes , NO of views , no of reposts , etc whatever you can . I want the output in json format {views: number, likes: number, retweets: number, replies: number}",
         url: ad.platformId,
-        webhook_callback_url: `https://warpads-agentic-hack.onrender.com/webhooks`,
+        webhook_callback_url: `https://warpads-cookie-hack.onrender.com//webhooks`,
       },
       {
         headers: {
