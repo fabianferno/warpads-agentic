966af720e43c3c7593d505a87ffa7cd82f03142b
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2025-02-07 20:39:53 +0530

    chore: Added Webhook for skyvern

--- indexer/src/routes/index.ts
+++ indexer/src/routes/index.ts
@@ -5,7 +5,6 @@ import { authMiddleware } from "../middleware/authMiddleware";
 import { trackUsage } from "../utilities/adEngine/trackUsage";
 import { trackResponse } from "../utilities/adEngine/trackResponse";
 import { getAllAgents } from "../utilities/GetAllAgents";
-import { calculateIncentive } from "../utilities/IncentiveCalculator";
 
 const router = Router();
 
@@ -20,16 +19,6 @@ router.get("/get-ad", authMiddleware, async (req: Request, res: Response) => {
   if (ad?.includes("No ad found")) {
     res.status(404).send("No ad found");
   } else {
-    //Track the response here itself  ( For Hackathon Purpose. Usually done via callback from the agent.)
-    const adSpaceId = await trackResponse(req.headers["x-api-key"] as string, {
-      platform: "twitter",
-      id: "1",
-    });
-    // Once the response is tracked , Calculate the incentive for the agent.
-
-    // Get the ad space id from the query
-    const incentive = await calculateIncentive(adSpaceId);
-    console.log(incentive);
     res.status(200).send(ad);
   }
 });
@@ -58,5 +47,10 @@ router.post(
     res.send("Response tracked");
   }
 );
+
+router.post("skyvern-webhook", async (req: Request, res: Response) => {
+  console.log(req.body);
+  res.send("Webhook received");
+});
 
 export default router;
